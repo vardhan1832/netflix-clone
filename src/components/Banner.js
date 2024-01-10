a7168b0e21e9bc29e92408e1cc7dc747550ecc9b
vardhan1831 <sreevardhan259@gmail.com>
2024-01-10 18:59:13 +0530

    successfully fetched movies from tmdb website using axios

---
+++ src/components/Banner.js
@@ -0,0 +1,43 @@
+import React , {useState,useEffect} from "react";
+import "./Banner.css";
+import axios from '../axios';
+import requests from "../Requests";
+const Banner = () => {
+    const [movie,setMovie] = useState([])
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + '...' : string;
+  }
+  useEffect(()=>{
+    async function fetchData(){
+        const response=await axios.get(requests.fetchNetflixOriginals);
+        setMovie(response.data.results[Math.floor(Math.random()*response.data.results.length-1)])
+        return response
+    }
+    fetchData()
+  },[])
+  console.log(movie)
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition:'center center'
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
+};
+
+export default Banner;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,73 +1,27 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import './HomeScreen.css'
-const HomeScreen = () =>{
-    return (
-        <div className="homeScreen">
-            <Navbar/>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-            <h1>testing</h1>
-        </div>
-    )
-}
+import Banner from "../components/Banner";
+import "./HomeScreen.css";
+import requests from "../Requests";
+const HomeScreen = () => {
+  return (
+    <div className="homeScreen">
+      <Navbar />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
